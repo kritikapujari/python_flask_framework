846896b3301bc410ab8aeace791e73ca12b60ff1
kritikapujari <kritikapujari05@gmail.com>
2026-04-24 23:08:50 +0530

    Initial commit with README

---
+++ getpost.py
@@ -0,0 +1,30 @@
+
+from flask import Flask,request,render_template
+'''
+It creates an instance of the Flask class,
+which will be your WSGI(Web Server Gateway Interface) application.
+'''
+#WSGI Application
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "<html><H1>Welcome to this webpage</H1></html>"
+@app.route("/index")
+def index():
+   return "Welcome to index page"
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/form",methods=['GET','POST'])
+def form():
+    if request.method=='POST':
+        name=request.form['name']
+        return f'Hello {name}'
+    return render_template('form.html')
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
